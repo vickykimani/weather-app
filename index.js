46d92fe92af25ccd91fba1e53e0eb28d0fb3de98
vickykimani <victorianightingale70@gmail.com>
2023-04-21 00:43:32 +0300

    search function

--- index.js
+++ index.js
@@ -22,9 +22,57 @@ search.addEventListener('click', () => {
             return;
         }
 
+        error404.style.display = 'block';
+        error404.classList.remove('fadeIn');
 
-    })
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-box .humidity span');
+        const wind = document.querySelector('.weather-box .wind span');
+
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/cloud.png';
+                break;
+
+            case 'Mist':
+                image.src = 'images/mist.png';
+                break;
+
+            default:
+                image.src = '';
+        }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.main.wind)}Km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '590px'
 
 
 
-})
+
+    });
+
+
+
+});
