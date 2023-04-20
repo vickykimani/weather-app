c576ffa180a6c7aebb7604c5bfe47c7e91aa794e
vickykimani <victorianightingale70@gmail.com>
2023-04-21 00:16:32 +0300

    api url

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '0b36c0609cf98dfe7006972161987449'
+    const APIKey = '0b36c0609cf98dfe7006972161987449';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=$ {APIKey}`).then(response => response.json()).then(json => {
+        if (json.cod === '404') {
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+        }
+
+
+    })
+
+
 
 })
