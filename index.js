d948777d207b2a99720c91b18afbd2c2f0e34152
vickykimani <victorianightingale70@gmail.com>
2023-04-21 00:04:52 +0300

    APIKey

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = '0b36c0609cf98dfe7006972161987449'
+
+})
